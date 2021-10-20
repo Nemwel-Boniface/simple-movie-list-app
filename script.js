630a336fe1c9189c868a0bc0cf0d2ac16437887b
Nemwel Boniface <nemwelboniface@outlook.com>
2021-10-20 17:05:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,51 @@
 document.addEventListener('DOMContentLoaded', function()
 {
-    var list = document.querySelector('.movie-list ul');
-    var form = document.forms;
+    const list = document.querySelector('.movie-list ul');
+    const form = document.forms;
     
     //logic for deleting movies
     
     list.addEventListener('click', (event)=> {
         if(event.target.className == 'delete')
             {
-                var li = event.target.parentElement;
+                const li = event.target.parentElement;
                 li.parentNode.removeChild(li);
             }
+        
+        //Logic for adding new elements
+        
+        const addform = form['add-movie'];
+        addform.addEventListener('submit', function(event){
+            event.preventDefault();
+            
+            //Create the elements
+            
+            const value = addform.querySelector('input[type="text"]').value;
+            const li = document.createElement('li');
+            const moviename = document.createElement('span');
+            const deletebtn = document.createElement('span');
+            
+            //add the text content
+            
+            moviename.textContent = value;
+            deletebtn.textContent = 'Delete';
+            
+            //add the styling(classes)
+            
+            moviename.classList.add('name');
+            deletebtn.classList.add('delete');
+            
+            //append to the DOM
+            
+            li.appendChild(moviename);
+            li.appendChild(deletebtn);
+            list.appendChild(li);
+             
+        });
+          
     });
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+      
     
 })
-
-
-
-
-
 
 
